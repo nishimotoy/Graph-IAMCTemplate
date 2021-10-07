4a7b4de8f136d76871eb0f6c820d1904360e9284
nishimoto <nishimoto.kyotou@gmail.com>
2021-10-07 19:02:42 +0900

    loop by graph setting

--- Plot_Indicators.R
+++ Plot_Indicators.R
@@ -24,39 +24,26 @@ df_all <- rbind(gather(df_past, key="Year", value="Value", -title),
 df_all$Year <- as.numeric(df_all$Year)
 view(df_all)
 
-# df_yokogata <- spread(df_all, key="Year",value="Value")
-# view(df_yokogata)
 
-
-#指標名とシナリオ名 で繰り返し処理 -------------------------------------------------------
+#指標名とシナリオ名 で繰り返し処理＠グラフ出力 -------------------------------------------------------
 
 indicators <- c("GDP_Capita","Electricity_Rate_Total","ChangeRate_Electricity_Rate_Total")
 scenarionames <- c("Baseline")    # c("Baseline","2C","1.5C","2.5C","WB2C")
 
+x_names <- c("Year","Year","Year","GDP_Capita","GDP_Capita")
+y_names <- c("GDP_Capita","Electricity_Rate_Total","ChangeRate_Electricity_Rate_Total",
+                          "Electricity_Rate_Total","ChangeRate_Electricity_Rate_Total")
+
 for (scenarioname in scenarionames) {
-  # view(scenarioname)
 
   df_forMerge <- select(df_all, c("REGION","Year",)
            ) %>% distinct()
   view(df_forMerge)
   
   for (indicator in indicators) {
-    # view(indicator)
-  
+
     df_Graph <- filter(df_all, VARIABLE==indicator, SCENARIO %in% c("Historical",scenarioname))
     View(df_Graph)
-
-    g <- ggplot(df_Graph, aes(x=Year, y=Value, color=REGION, shape=SCENARIO)) +
-      geom_line() +
-      geom_point() + 
-      scale_shape_manual(values=c(19,21)) +
-      ylab(indicator)
-    plot(g)
-    filename <- paste(scenarioname,"_Year-",indicator)
-    ggsave(file=paste("./../4_output/",filename,".pdf"))
-
-    # df_Graph_tmp1 <- rename(df_Graph, GDP_Capita=Value)
-
     df_Graph_tmp <- select(df_Graph, -c("MODEL","UNIT","VARIABLE"))
     df_Graph_tmp <- eval(parse(text=paste0("rename(df_Graph_tmp,", indicator, "=Value)")))
     View(df_Graph_tmp)
@@ -65,30 +52,17 @@ for (scenarioname in scenarionames) {
     View(df_forMerge)
   }
 
-  # GDPcapita vs. ER, CRER プロット用のdf作成
-
-  # x=GDP_Capita, y=Electricity_Rate_Total
-  g <- ggplot(df_forMerge, aes(x=GDP_Capita, y=Electricity_Rate_Total, color=REGION, shape=SCENARIO)) +
-    geom_line() +
-    geom_point() + 
-    scale_shape_manual(values=c(19,21)) 
-  plot(g)
-  filename <- paste(scenarioname,"_GDPcapita-Electricity_Rate")
-  ggsave(file=paste("./../4_output/",filename,".pdf"))
-
-
   # グラフ描画 by XYのテキスト指定
+  for (num in 1:length(x_names)) {
 
-  xname <- "GDP_Capita"
-  yname <- "Electricity_Rate_Total"
-  
-  g <- eval(parse(text=paste0(
-            "ggplot(df_forMerge, aes(x=",xname,",y=",yname, ",color=REGION,shape=SCENARIO)) +
-    geom_line() +
-    geom_point() + 
-    scale_shape_manual(values=c(19,21))")))
-  plot(g)
-  filename <- paste(scenarioname,"_",xname,"-",yname)
-  ggsave(file=paste("./../4_output/",filename,".png"))
-  
+    g <- eval(parse(text=paste0(
+            "ggplot(df_forMerge, aes(x=",x_names[num],",y=",y_names[num], 
+            ",color=REGION,shape=SCENARIO)) +
+        geom_line() +
+        geom_point() + 
+        scale_shape_manual(values=c(19,21))")))
+      plot(g)
+      filename <- paste(scenarioname,"_",x_names[num],"-",y_names[num])
+      ggsave(file=paste("./../4_output/",filename,".png"))
+  }  
 }
