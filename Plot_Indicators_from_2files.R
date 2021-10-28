b4f6de6ace1bd219813f6cb14f7d7dd43bdbad06
nishimoto <nishimoto.kyotou@gmail.com>
2021-10-28 15:56:46 +0900

    Output to 4 types of Graph.

--- Plot_Indicators_from_2files.R
+++ Plot_Indicators_from_2files.R
@@ -1,23 +1,22 @@
 #Packages------------------------------------------------------
-# update.packages("ggplot2")
 library(ggplot2)
-# update.packages("tidyverse")
 library(tidyverse)
+library(patchwork)
 
-getwd() 
-# setwd("c:/usr") 
+# getwd() 
+setwd("C:/_Nishimoto/R/WBAL_R02/") 
 
 # データの読み込み---------------------------------------------------------------
-df_past <- read_csv("./../2_data/REF2/IAMCTemplate_i_past.csv")
+df_past <- read_csv("./2_data/REF2/IAMCTemplate_i_past.csv")
 # view(df_past)
 
-df_future <- read_csv("./../2_data/REF2/IAMCTemplate_i_future.csv")
+df_future <- read_csv("./2_data/REF2/IAMCTemplate_i_future.csv")
 # view(df_future)
 
 titlerow <- c("MODEL","SCENARIO","REGION","VARIABLE","UNIT")
 
-df_all <- rbind(gather(df_past, key="Year", value="Value", -titlerow ),
-                gather(df_future, key="Year", value="Value", -titlerow )
+df_all <- rbind(gather(df_past, key="Year", value="Value", -all_of(titlerow) ),
+                gather(df_future, key="Year", value="Value", -all_of(titlerow) )
     ) %>% mutate(SCENARIO=gsub("Reference","Historical",SCENARIO)
     ) %>% filter(MODEL != "MODEL"
     ) %>% na.omit() 
@@ -41,6 +40,7 @@ x_names <- c(rep("Year",length(indicators)),
 y_names <- c(indicators,
              indicators[-1],
              indicators[-1])
+y2_names <- indicators[c(3,5,7)]
 
 for (scenarioname in scenarionames) {
   
@@ -53,13 +53,13 @@ for (scenarioname in scenarionames) {
     ) %>% select(-c('MODEL','UNIT','VARIABLE'))
     df_Graph <- eval(parse(text=paste0("rename(df_Graph,", indicator, "=Value)")))
     df_Graph <- df_Graph[order(df_Graph$Year),]
+    View(df_Graph)
     df_forMerge <- merge(df_forMerge, df_Graph)
-    View(df_forMerge)
-    
   }
+  View(df_forMerge)
   
-  # グラフ描画 by XYのテキスト指定
-  pdf(file=paste("./../4_output/figures_",scenarioname,".pdf", sep=""))    
+  # XY散布図 by 軸名のテキスト指定
+  pdf(file=paste("./4_output/XY_",scenarioname,".pdf", sep=""))    
   for (num in 1:length(x_names)) {
     
     g <- eval(parse(text=paste0(
@@ -70,7 +70,71 @@ for (scenarioname in scenarionames) {
         scale_shape_manual(values=c(19,21))")))
     plot(g)
     filename <- paste(scenarioname,num,"_",x_names[num],"-",y_names[num], sep="")
-    # ggsave(file=paste("./../4_output/",filename,".png"))
+    # ggsave(file=paste("./4_output/",filename,".png"))
   }
   dev.off() 
+
+  # 4in1 レイアウト
+  pdf(file=paste("./5_test/figures_",scenarioname,".pdf", sep=""))    
+  for (num in 1:length(x_names)) {
+    
+    p1 <- eval(parse(text=paste0(
+      "ggplot(df_forMerge, aes(x=",x_names[num],",y=",y_names[num], 
+      ",color=REGION,shape=SCENARIO)) +
+        geom_line() +
+        geom_point() + 
+        scale_shape_manual(values=c(19,21))")))
+    
+    if ( num%%4==1 ) { p <- p1 } else { p <- p + p1 }
+    if ( num%%4==0 ) { plot(p) } 
+  }
+  dev.off() 
+  
+  # バイオリン
+  pdf(file=paste("./4_output/violin_",scenarioname,".pdf", sep=""))    
+  for (indicator in indicators) {
+
+    g <- eval(parse(text=paste0(
+      "ggplot(df_forMerge, aes(x=","REGION",",y=",indicator, 
+      ",color=SCENARIO)) +
+        geom_violin() + 
+        geom_jitter(shape=20, position=position_dodge(1.0))")))
+    plot(g)
+  }
+  dev.off() 
+  
+  # 箱ヒゲ図
+  pdf(file=paste("./4_output/boxplot_",scenarioname,".pdf", sep=""))    
+  for (indicator in indicators) {
+    
+    g <- eval(parse(text=paste0(
+      "ggplot(df_forMerge, aes(x=","REGION",",y=",indicator, 
+      ",color=SCENARIO)) +
+        geom_boxplot() + 
+        geom_jitter(shape=20, position=position_dodge(0.8))")))
+    plot(g)
+  }
+  dev.off() 
+  
+  # 頻度分布
+  pdf(file=paste("./4_output/histogram_",scenarioname,".pdf", sep=""))    
+  for (indicator in indicators) {
+    
+    g <- eval(parse(text=paste0(
+      "ggplot(df_forMerge, aes(x=",indicator, 
+      ",color=SCENARIO)) +
+        geom_histogram(bins=50) +
+        ylab('Count of Region-Year')")))
+    plot(g)
+  }
+  dev.off() 
+  
 }
+
+# 頻度分布を作る
+# g <- ggplot(df_forMerge, aes(x = ChangeRate_Electricity_Rate_Total))
+# g <- g + geom_histogram(binwidth=0.005) # set histogram chart
+# g <- g + xlab("Class value (cm)") # x label
+# g <- g + ylab("Frequency")        # y label 
+# g <- g + ggtitle("Histogram by ggplot") # title
+# plot(g)  
