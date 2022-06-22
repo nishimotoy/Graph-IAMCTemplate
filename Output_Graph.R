0a5efe55653ab84bdfc1d47730fe2b41ca7bea0f
nishimoto <nishimoto.kyotou@gmail.com>
2022-06-22 18:02:57 +0900

    Reindent

--- Output_Graph.R
+++ Output_Graph.R
@@ -46,98 +46,98 @@ df_Graph_global_wide <- df_Graph_global %>% spread(key=SCENARIO_f, value=CO2_fue
 write_csv(df_Graph_global, "./df_Graph_global_written.csv") 
 write_csv(df_Graph_global_wide, "./df_Graph_global_wide_written.csv") 
 
-  pdf(file=paste("./png2/JSCE_Graph.pdf", sep=""))    
-  for (num in 1:length(x_names)) { #num # XYグラフの出力
-
-    if ( num==1 ) {
-      df_Graph_plot <- df_Graph_global %>% filter(SCENARIO_f!='Historical' 
-                                     ) %>% mutate(CO2_fuel_Total_scaled=CO2_fuel_Total_scaled/1000000) #kt>Gt-CO2
-
-      g <- eval(parse(text=paste0("
+pdf(file=paste("./png2/JSCE_Graph.pdf", sep=""))    
+for (num in 1:length(x_names)) { #num # XYグラフの出力
+  
+  if ( num==1 ) {
+    df_Graph_plot <- df_Graph_global %>% filter(SCENARIO_f!='Historical' 
+    ) %>% mutate(CO2_fuel_Total_scaled=CO2_fuel_Total_scaled/1000000) #kt>Gt-CO2
+    
+    g <- eval(parse(text=paste0("
               ggplot(df_Graph_plot, aes(x=",x_names[num],",y=",y_names[num], 
-                                  ",color=SCENARIO)) +
+                                ",color=SCENARIO)) +
               geom_line(size=1.6) +
               scale_color_manual(values=c(scenario_color[-1])) 
       "))) 
-      plot(g)
-      
-    } else if ( num>=2 && num<=4 ) { 
-      
-      df_Graph_plot <- df_Graph_p %>% filter(SCENARIO_f %in% c('Historical_R17', 'Baseline'))
-      
-      g <- eval(parse(text=paste0("
+    plot(g)
+    
+  } else if ( num>=2 && num<=4 ) { 
+    
+    df_Graph_plot <- df_Graph_p %>% filter(SCENARIO_f %in% c('Historical_R17', 'Baseline'))
+    
+    g <- eval(parse(text=paste0("
               ggplot(df_Graph_plot, aes(x=",x_names[num],",y=",y_names[num], 
-                                  ",color=REGION, shape=SCENARIO)) +
+                                ",color=REGION, shape=SCENARIO)) +
               geom_line() +
               geom_point() + 
               scale_color_manual(values=c(rep(region_color,3))) +
               scale_shape_manual(values=c(19,21))"))) # 'Historical_R17', 'Baseline'
-      plot(g)
-      
-    } else if ( num>=5 && num<=10 ) { # XY散布図 by 17地域 vs 17地域
-      
-      df_Graph_plot <- df_Graph_p %>% filter(SCENARIO_f!='Historical')
-      df_Graph_plot_HisR <- df_Graph_plot %>% filter(SCENARIO_f=='Historical_R17' )
-      
-      g <- eval(parse(text=paste0(
-        "ggplot(df_Graph_plot, aes(x=",x_names[num],",y=",y_names[num], 
-        ",color=REGION, shape=SCENARIO)) +
+    plot(g)
+    
+  } else if ( num>=5 && num<=10 ) { # XY散布図 by 17地域 vs 17地域
+    
+    df_Graph_plot <- df_Graph_p %>% filter(SCENARIO_f!='Historical')
+    df_Graph_plot_HisR <- df_Graph_plot %>% filter(SCENARIO_f=='Historical_R17' )
+    
+    g <- eval(parse(text=paste0(
+      "ggplot(df_Graph_plot, aes(x=",x_names[num],",y=",y_names[num], 
+      ",color=REGION, shape=SCENARIO)) +
               geom_point() + 
               geom_line() +
               scale_color_manual(values=c(rep(region_color,3))) +
               scale_shape_manual(values=c(19,21,22,23,24,25,1))"))) # SCENARIO数
+    
+    if ( num>=8 && num<=10 ) { # 窓の追加
       
-      if ( num>=8 && num<=10 ) { # 窓の追加
-        
-        vec_data <- eval(parse(text=paste0("df_Graph_plot_HisR$",y_names[num]))) 
-        percentile_val <- percentitle_range(vec_data, cutoff_percentile)
-        g <- g + eval(parse(text=paste0( "annotate('rect', xmin=",-Inf,", ymin=", percentile_val[1], 
-                                         ", xmax=",Inf, ", ymax=",percentile_val[2], 
-                                         ", alpha=.125, fill='#329262')"))) 
-        
-      } # 窓の追加
-      if ( num==9 ) { g <- g + ylim(-10, 5) } #炭素強度の例外処理
-      plot(g)
-
-    } else if ( num>=11 && num<=13 ) { # 確率密度分布
+      vec_data <- eval(parse(text=paste0("df_Graph_plot_HisR$",y_names[num]))) 
+      percentile_val <- percentitle_range(vec_data, cutoff_percentile)
+      g <- g + eval(parse(text=paste0( "annotate('rect', xmin=",-Inf,", ymin=", percentile_val[1], 
+                                       ", xmax=",Inf, ", ymax=",percentile_val[2], 
+                                       ", alpha=.125, fill='#329262')"))) 
       
-      df_Graph_plot <- df_Graph_p
-      df_Graph_plot_HisR <- df_Graph_plot %>% filter(SCENARIO_f=='Historical_R17' )
-
-      indicator <- x_names[num]
-
-      g <- eval(parse(text=paste0(
-          "ggplot(df_Graph_plot, aes(x=",indicator, ",color=SCENARIO)) +
+    } # 窓の追加
+    if ( num==9 ) { g <- g + ylim(-10, 5) } #炭素強度の例外処理
+    plot(g)
+    
+  } else if ( num>=11 && num<=13 ) { # 確率密度分布
+    
+    df_Graph_plot <- df_Graph_p
+    df_Graph_plot_HisR <- df_Graph_plot %>% filter(SCENARIO_f=='Historical_R17' )
+    
+    indicator <- x_names[num]
+    
+    g <- eval(parse(text=paste0(
+      "ggplot(df_Graph_plot, aes(x=",indicator, ",color=SCENARIO)) +
             geom_density(size=0.7) +
             scale_color_manual(values=c(scenario_color)) +
             xlim(-10,10) + 
             ylab('Density (Counts scaled to 1) of Region-Year')")))
-        
-        vec_data <- eval(parse(text=paste0("df_Graph_plot_HisR$",indicator))) 
-        percentile_val <- percentitle_range(vec_data, cutoff_percentile)
-        g <- g + eval(parse(text=paste0( "annotate('rect', xmin=",percentile_val[1],", ymin=",-Inf, 
-                                         ", xmax=",percentile_val[2], ", ymax=",Inf, 
-                                         ", alpha=.2, fill='#329262')"))) 
-        plot(g)
-
-    }  # 確率密度分布
     
-    if ( y_names_J[num]=='エネルギー起源CO2排出量' ) { 
-      ylab_name <- bquote("エネルギー起源   " ~ CO[2] ~ "排出量  " ~ (Gt-CO[2])) #bquote内では 変数名は文字列とされる
-    } else if ( y_names_J[num]=='エネルギー強度' ) { ylab_name <- paste(y_names_J[num], '(MJ/$)')
-    } else if ( y_names_J[num]=='炭素強度' )     { ylab_name <- bquote('炭素強度　' ~ (10^-8 ~ CO[2]/J) )
-    } else if ( y_names_J[num]=='電化率' )       { ylab_name <- paste(y_names_J[num], '(%)')
-    } else { ylab_name <-  y_names_J[num] }
-
-    g <- g + xlab(x_names_J[num]) + ylab(ylab_name) + theme_bw() + theme(panel.grid = element_blank()) # + MyThemeLine
+    vec_data <- eval(parse(text=paste0("df_Graph_plot_HisR$",indicator))) 
+    percentile_val <- percentitle_range(vec_data, cutoff_percentile)
+    g <- g + eval(parse(text=paste0( "annotate('rect', xmin=",percentile_val[1],", ymin=",-Inf, 
+                                     ", xmax=",percentile_val[2], ", ymax=",Inf, 
+                                     ", alpha=.2, fill='#329262')"))) 
     plot(g)
-
-    filename <- paste("JSCE",num,"_",x_names[num],"-",y_names[num], sep="") # 土木学会用出力
-    ggsave(file=paste("./png2/",filename,".png", sep=""), width=5, height=4, dpi=100)
-    ggsave(file=paste("./png3/",filename,".png", sep=""), width=5, height=7, dpi=100) # 凡例の出力
     
-  }  #num # XYグラフの出力
+  }  # 確率密度分布
   
+  if ( y_names_J[num]=='エネルギー起源CO2排出量' ) { 
+    ylab_name <- bquote("エネルギー起源   " ~ CO[2] ~ "排出量  " ~ (Gt-CO[2])) #bquote内では 変数名は文字列とされる
+  } else if ( y_names_J[num]=='エネルギー強度' ) { ylab_name <- paste(y_names_J[num], '(MJ/$)')
+  } else if ( y_names_J[num]=='炭素強度' )     { ylab_name <- bquote('炭素強度　' ~ (10^-8 ~ CO[2]/J) )
+  } else if ( y_names_J[num]=='電化率' )       { ylab_name <- paste(y_names_J[num], '(%)')
+  } else { ylab_name <-  y_names_J[num] }
+  
+  g <- g + xlab(x_names_J[num]) + ylab(ylab_name) + theme_bw() + theme(panel.grid = element_blank()) # + MyThemeLine
+  plot(g)
+  
+  filename <- paste("JSCE",num,"_",x_names[num],"-",y_names[num], sep="") # 土木学会用出力
+  ggsave(file=paste("./png2/",filename,".png", sep=""), width=5, height=4, dpi=100)
+  ggsave(file=paste("./png3/",filename,".png", sep=""), width=5, height=7, dpi=100) # 凡例の出力
+  
+}  #num # XYグラフの出力
+
 
 for (indicator in y_names_box) { # indicator # 箱ヒゲ図
   df_Graph_plot <- df_Graph_p 
