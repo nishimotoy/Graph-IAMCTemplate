4878eb3e79c39641a69a4601718559bca6eb007b
GOLE1-7S64LR4\Yu <nishimoto.prog@gmail.com>
2023-04-18 03:00:12 +0900

    file name "cor.txt" changed df name "df_His_R_item" changed

--- cor.R
+++ cor.R
@@ -11,17 +11,17 @@
                       , 'Henkaryo_Electricity_Rate_Total'
   )
 
-  cat(" START cor(Year, item) \n\n", file="lm2.txt", append=F)
+  cat(" START cor(Year, item) \n\n", file="cor.txt", append=F)
   df_His_R <- df_Graph %>% filter(SCENARIO=='Historical_R17') 
   attach(df_His_R)
   # item <- 'ChangeRate_Energy_Intensity' #  ", item, "
   for (item in test_items) {
-    df_His_R_selected <- eval(parse(text=paste0("df_His_R %>% select(Year, ", item, ")"))) 
+    df_His_R_item <- eval(parse(text=paste0("df_His_R %>% select(Year, ", item, ")"))) 
     cor_year_item <- eval(parse(text=paste0(" cor(Year, ", item, ", use='complete.obs')"))) 
     cat(" item=", item, "\n", 
         "cor_year_item=", cor_year_item, "\n\n", 
-        file="lm2.txt", append=TRUE)
+        file="cor.txt", append=TRUE)
   }
-  cat(" END \n", file="lm2.txt", append=TRUE)
+  cat(" END \n", file="cor.txt", append=TRUE)
   detach(df_His_R)
   
